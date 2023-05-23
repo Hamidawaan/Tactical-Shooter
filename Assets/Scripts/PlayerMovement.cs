94ed72ca430d60e333b635f7d6124219c95c9d66
HamidAwan <muhammadhamidawancs@gmail.com>
2023-05-23 13:38:21 -0700

    Project Files

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,16 +16,16 @@ public class PlayerMovement : MonoBehaviour
         playerMove();
     }
 
-    void playerMove()
+    void playerMove()   //(normalized) the magnitude of the direction vector is always between 0 and 1.
     {
         float horizontal_axis = Input.GetAxisRaw("Horizontal");
         float Vertical_axis = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(horizontal_axis, 0f,  Vertical_axis).normalized;
         
-        if(direction.magnitude >= 0.1f)
+        if(direction.magnitude >= 0.1f)//magnitude is to calculate the length of the vector
         {
-            cC.Move(direction.normalized * playerSpeed * Time.deltaTime);
+            cC.Move(direction.normalized * playerSpeed * Time.deltaTime);//Time.deltaTime to ensure consistent movement speed across different frame rates.
         }
     }
 

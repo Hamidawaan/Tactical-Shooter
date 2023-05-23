376de2a4661ffe262acbcd11743f02d418ed602c
HamidAwan <muhammadhamidawancs@gmail.com>
2023-05-23 13:59:04 -0700

    Project Files

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,12 +21,15 @@ public class PlayerMovement : MonoBehaviour
         float horizontal_axis = Input.GetAxisRaw("Horizontal");
         float Vertical_axis = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal_axis, 0f,  Vertical_axis).normalized;
-        
-        if(direction.magnitude >= 0.1f)//magnitude is to calculate the length of the vector
-        {
-            cC.Move(direction.normalized * playerSpeed * Time.deltaTime);//Time.deltaTime to ensure consistent movement speed across different frame rates.
-        }
-    }
+        Vector3 direction = new Vector3(horizontal_axis, 0f, Vertical_axis).normalized;
 
+        if (direction.magnitude >= 0.1f)//magnitude is to calculate the length of the vector
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            cC.Move(direction.normalized * playerSpeed * Time.deltaTime);//Time.deltaTime to ensure consistent movement speed across different frame rates. //  that represents the time in seconds
+                                                                         //direction.normalized is used to normalize the direction vector before applying it to the player's movement. This ensures that the player moves at a consistent speed
+        }
+
+    }
 }

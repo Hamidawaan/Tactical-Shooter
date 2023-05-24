d4559549d8be06e03e91583cedc81acbd570980f
HamidAwan <muhammadhamidawancs@gmail.com>
2023-05-24 13:37:00 -0700

    Project Files

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,14 +14,37 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Player Animator and Gravity")]
     public CharacterController cC;
-
+    public float gravity = -9.81f;
 
     [Header("Player jumping and velocity")]
+    public float jumpRange = 1f;
     public float turnCalmTime = 0.1f;
     float turnCalmVelocity;
+    Vector3 velocity;
+    public Transform surfaceCheck;
+    bool onSurface;
+    public float surfaceDistance = 0.4f;
+    public LayerMask surfaceMask;
+
+
+
+
     private void Update()
     {
+        //if the character is on a surface, applies a downward force to keep it grounded,
+        //updates the velocity with gravity, and finally moves the character based on the updated velocity.
+        onSurface = Physics.CheckSphere(surfaceCheck.position, surfaceDistance, surfaceMask);
+
+        if(onSurface && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        cC.Move(velocity * Time.deltaTime);
+
         playerMove();
+        Jump();
     }
 
     void playerMove()   //(normalized) the magnitude of the direction vector is always between 0 and 1.
@@ -33,7 +56,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)//magnitude is to calculate the length of the vector
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg *playerCamera.eulerAngles.y;// calculates the target angle based on the direction vector and sets the object's rotation to face that angle in a single line.  
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;// calculates the target angle based on the direction vector and sets the object's rotation to face that angle in a single line.  
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime );// used to smoothly rotate objects, such as a character or camera, to a desired angle over time,
 
             
@@ -42,6 +65,16 @@ public class PlayerMovement : MonoBehaviour
             cC.Move(moveDirection.normalized * playerSpeed * Time.deltaTime);//Time.deltaTime to ensure consistent movement speed across different frame rates. //  that represents the time in seconds
                                                                          //direction.normalized is used to normalize the direction vector before applying it to the player's movement. This ensures that the player moves at a consistent speed
         }
+
+    }
+
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && onSurface)
+        velocity.y = Mathf.Sqrt(jumpRange * -2 * gravity);
+
+
 
     }
 }

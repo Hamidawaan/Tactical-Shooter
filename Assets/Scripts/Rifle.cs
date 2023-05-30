b9ac851a48c8bf1fa4d307edddc7dfc37d67fe7c
HamidAwan <muhammadhamidawancs@gmail.com>
2023-05-30 06:23:32 -0700

    Project Files

--- Assets/Scripts/Rifle.cs
+++ Assets/Scripts/Rifle.cs
@@ -8,11 +8,16 @@ public class Rifle : MonoBehaviour
     public Camera cam;
     public float giveDamage = 10f;
     public float shootingRange = 100f;
+    public float fireCharge = 15f;
+
+    [Header("Rifle Annimation and Shooting")]
+    private float nextTimeToShoot = 0f;
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1")  && Time.time >= nextTimeToShoot)
         {
+            nextTimeToShoot = Time.time + 1f / fireCharge;  // Time.time is the current time. //   the 1f/firecharge means the rifle shoot raycst will be 0.06
             shoot();
         }
     }
